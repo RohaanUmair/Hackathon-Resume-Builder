358851fe825d70a30e393917deafe315891ee1ab
RohaanUmair <m.rohaanumair@gmail.com>
2024-09-08 08:19:33 +0500

    Milestone 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 var skillsSection = document.querySelector('.skills_section');
 var toggleSkillsBtn = document.querySelector('#toggle_skills_btn');
+var form = document.querySelector('form');
+var resume = document.querySelector('.resume');
+var form_fullname = document.getElementById('form_fullname');
+var form_email = document.getElementById('form_email');
+var form_contact = document.getElementById('form_contact');
+var form_secondary_edu = document.getElementById('form_secondary_edu');
+var form_primary_edu = document.getElementById('form_primary_edu');
+var form_comapny = document.getElementById('form_comapny');
+var form_exp = document.getElementById('form_exp');
+var form_main_skill = document.getElementById('form_main_skill');
+var form_sub_skills = document.getElementById('form_sub_skills');
 var showSkills = false;
 toggleSkillsBtn === null || toggleSkillsBtn === void 0 ? void 0 : toggleSkillsBtn.addEventListener(('click'), (function () {
     if (showSkills) {
@@ -12,4 +23,18 @@ toggleSkillsBtn === null || toggleSkillsBtn === void 0 ? void 0 : toggleSkillsBt
         toggleSkillsBtn.innerText = 'Hide';
         showSkills = true;
     }
+}));
+form.addEventListener(('submit'), (function (e) {
+    e.preventDefault();
+    resume.children[1].children[1].innerHTML = form_fullname.value;
+    resume.children[1].children[3].innerHTML = form_email.value;
+    resume.children[1].children[5].innerHTML = form_contact.value;
+    resume.children[3].children[1].innerHTML = form_secondary_edu.value;
+    resume.children[3].children[3].innerHTML = form_primary_edu.value;
+    resume.children[5].children[0].innerHTML = form_comapny.value;
+    resume.children[5].children[1].innerHTML = form_exp.value;
+    resume.children[7].children[0].children[0].innerHTML = form_main_skill.value;
+    resume.children[7].children[0].children[1].innerHTML = form_sub_skills.value;
+    resume.style.display = 'block';
+    form.style.display = 'none';
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 const skillsSection = document.querySelector('.skills_section') as HTMLDivElement;
 const toggleSkillsBtn = document.querySelector('#toggle_skills_btn') as HTMLButtonElement;
 
+const form = document.querySelector('form') as HTMLFormElement;
+const resume = document.querySelector('.resume') as HTMLDivElement;
+
+const form_fullname = document.getElementById('form_fullname') as HTMLInputElement;
+const form_email = document.getElementById('form_email') as HTMLInputElement;
+const form_contact = document.getElementById('form_contact') as HTMLInputElement;
+const form_secondary_edu: any = document.getElementById('form_secondary_edu') as HTMLInputElement;
+const form_primary_edu = document.getElementById('form_primary_edu') as HTMLInputElement;
+const form_comapny = document.getElementById('form_comapny') as HTMLInputElement;
+const form_exp = document.getElementById('form_exp') as HTMLInputElement;
+const form_main_skill = document.getElementById('form_main_skill') as HTMLInputElement;
+const form_sub_skills = document.getElementById('form_sub_skills') as HTMLInputElement;
+
+
+
+
 
 let showSkills: boolean = false;
 toggleSkillsBtn?.addEventListener(('click'), (() => {
@@ -13,4 +29,26 @@ toggleSkillsBtn?.addEventListener(('click'), (() => {
         toggleSkillsBtn.innerText = 'Hide';
         showSkills = true;
     }
+}));
+
+
+
+form.addEventListener(('submit'), ((e) => {
+    e.preventDefault();
+    
+    resume.children[1].children[1].innerHTML = form_fullname.value;
+    resume.children[1].children[3].innerHTML = form_email.value;
+    resume.children[1].children[5].innerHTML = form_contact.value;
+    
+    resume.children[3].children[1].innerHTML = form_secondary_edu.value;
+    resume.children[3].children[3].innerHTML = form_primary_edu.value;
+    
+    resume.children[5].children[0].innerHTML = form_comapny.value;
+    resume.children[5].children[1].innerHTML = form_exp.value;
+    
+    resume.children[7].children[0].children[0].innerHTML = form_main_skill.value;
+    resume.children[7].children[0].children[1].innerHTML = form_sub_skills.value;
+    
+    resume.style.display = 'block';
+    form.style.display = 'none';
 }));
